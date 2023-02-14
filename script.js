8a347257cd45d4794bb87d1fa9bc6b564012b830
Niloy Chowdhury <125306577+NiloyChowdhuryVR@users.noreply.github.com>
2023-02-14 16:49:53 +0530

    Fixed bugs in 5 letter hex numbers

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ document.getElementById('btn').addEventListener('click',random);
 function random(){
     var randomNum = Math.floor(Math.random() * 16777215);
     var hexNum = '#' + randomNum.toString(16);
-    document.body.style.backgroundColor = hexNum;
+    if(hexNum.length == '7'){
+        document.body.style.backgroundColor = hexNum;
+    }
+    else{
+        document.body.style.backgroundColor = hexNum + '0';
+    }
     document.getElementById('hex').innerHTML = hexNum;
 }
 
